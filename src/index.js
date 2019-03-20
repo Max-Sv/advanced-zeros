aee7c33d00161e594eaf428fe9703cb031c91dd1
Max <mlalx@tut.by>
2019-03-20 19:50:13 +0300

    fix: for 10 base

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  
+  let numberZero = 0, n = 0;
+  for (let i = 0; ; i++) {
+    if (Math.pow(5, i) > number) {
+      n = i - 1;
+      break;
+    }
+  }
+  for (let index = 1; index <= n; index++) {
+    numberZero += Math.floor(number / Math.pow(5, index));
+  }
+  return numberZero;
 }
