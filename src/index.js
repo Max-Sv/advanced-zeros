3e44f1af27eec139cb0f3743dda82668205fa860
Max <mlalx@tut.by>
2019-03-21 13:41:23 +0300

    fix: add other dases

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 module.exports = function getZerosCount(number, base) {
-  
-  let numberZero = 0, n = 0;
-  for (let i = 0; ; i++) {
-    if (Math.pow(5, i) > number) {
-      n = i - 1;
-      break;
+  let numberZero = 100000000;
+  for (let i = 2; i <= base; i++) {
+    if (base % i == 0) {
+      let numDivision1 = 0;
+      while (base % i == 0) {
+        numDivision1++;
+        base /= i;
+      }
+      let numDivision2 = 0;
+      let t = number;
+      while (t / i > 0) {
+        numDivision2 = Math.floor(numDivision2 + t / i) ;
+        t /= i;
+      }
+      numberZero = Math.min(numberZero, (Math.floor(numDivision2 / numDivision1)))
     }
-  }
-  for (let index = 1; index <= n; index++) {
-    numberZero += Math.floor(number / Math.pow(5, index));
+    
   }
   return numberZero;
 }
